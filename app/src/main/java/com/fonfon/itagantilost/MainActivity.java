d948d95a6c69a10b364186d079e0eb857ed619c8
Дмитрий Романов <rdfonfon@gmail.com>
2017-05-03 10:32:49 +0300

    initial commit

---
+++ app/src/main/java/com/fonfon/itagantilost/MainActivity.java
@@ -0,0 +1,13 @@
+package com.fonfon.itagantilost;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
